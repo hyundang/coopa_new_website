788c35e5e3f368db573a5a76af977bea673f5f38
chaerin <achrvv@sookmyung.ac.kr>
2021-08-18 19:42:13 +0900

    feat: 디렉토리 공유

---
+++ src/pages/share/[id].tsx
@@ -0,0 +1,199 @@
+import styled from "styled-components";
+import { Header } from "@components/organisms";
+import { CookieIcon } from "@assets/icons/common";
+import Cookies from "@components/templates/Cookies";
+
+const Share = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <ShareCntnr>
+        <Title>
+          <p className="name">playlist</p>
+          <p className="info">
+            <CookieIcon fill="#000000" />
+            8개
+          </p>
+        </Title>
+        <User>
+          <img
+            alt=""
+            src="https://lh4.googleusercontent.com/-8Sj3uh-4Tvc/AAAAAAAAAAI/AAAAAAAAAAA/AMZuucm1v42OfrKMBeMcncTbD27GToGVqA/s96-c/photo.jpg"
+          />
+          <p>희수 친구 채린</p>
+        </User>
+        <Cookies
+          data={[
+            {
+              content:
+                "이번 플리는 오래전부터 좋아했던 아티스트 'Justice Der'의 기타 커버곡으로 이루어져 있습니다. 음악을 듣다 보면 한번쯤은 들어보셨을 만한 유명한 팝송 위주로 선곡했습니다. 봄밤과 어울리는 은은한 분위기의 jazzy한 기타 커버곡 플레이리스트 좋아하셨으면 좋겠네요 :)+ 아...",
+              directoryInfo: {
+                emoji: null,
+                id: 1367,
+                name: "playlist🎧",
+              },
+              favicon:
+                "https://www.youtube.com/s/desktop/cb471591/img/favicon_32x32.png",
+              id: 11973,
+              link: "https://www.youtube.com/watch?v=C7sLwDOL1Vo",
+              provider: "YouTube",
+              readCnt: 1,
+              thumbnail: "https://i.ytimg.com/vi/C7sLwDOL1Vo/hqdefault.jpg",
+              title:
+                "[playlist]봄 밤, 창이 열린 1층 카페 끝 자리, 가사 없는 chilling 음악",
+            },
+            {
+              content:
+                "이번 플리는 오래전부터 좋아했던 아티스트 'Justice Der'의 기타 커버곡으로 이루어져 있습니다. 음악을 듣다 보면 한번쯤은 들어보셨을 만한 유명한 팝송 위주로 선곡했습니다. 봄밤과 어울리는 은은한 분위기의 jazzy한 기타 커버곡 플레이리스트 좋아하셨으면 좋겠네요 :)+ 아...",
+              directoryInfo: {
+                emoji: null,
+                id: 1367,
+                name: "playlist🎧",
+              },
+              favicon:
+                "https://www.youtube.com/s/desktop/cb471591/img/favicon_32x32.png",
+              id: 11973,
+              link: "https://www.youtube.com/watch?v=C7sLwDOL1Vo",
+              provider: "YouTube",
+              readCnt: 1,
+              thumbnail: "https://i.ytimg.com/vi/C7sLwDOL1Vo/hqdefault.jpg",
+              title:
+                "[playlist]봄 밤, 창이 열린 1층 카페 끝 자리, 가사 없는 chilling 음악",
+            },
+            {
+              content:
+                "이번 플리는 오래전부터 좋아했던 아티스트 'Justice Der'의 기타 커버곡으로 이루어져 있습니다. 음악을 듣다 보면 한번쯤은 들어보셨을 만한 유명한 팝송 위주로 선곡했습니다. 봄밤과 어울리는 은은한 분위기의 jazzy한 기타 커버곡 플레이리스트 좋아하셨으면 좋겠네요 :)+ 아...",
+              directoryInfo: {
+                emoji: null,
+                id: 1367,
+                name: "playlist🎧",
+              },
+              favicon:
+                "https://www.youtube.com/s/desktop/cb471591/img/favicon_32x32.png",
+              id: 11973,
+              link: "https://www.youtube.com/watch?v=C7sLwDOL1Vo",
+              provider: "YouTube",
+              readCnt: 1,
+              thumbnail: "https://i.ytimg.com/vi/C7sLwDOL1Vo/hqdefault.jpg",
+              title:
+                "[playlist]봄 밤, 창이 열린 1층 카페 끝 자리, 가사 없는 chilling 음악",
+            },
+            {
+              content:
+                "이번 플리는 오래전부터 좋아했던 아티스트 'Justice Der'의 기타 커버곡으로 이루어져 있습니다. 음악을 듣다 보면 한번쯤은 들어보셨을 만한 유명한 팝송 위주로 선곡했습니다. 봄밤과 어울리는 은은한 분위기의 jazzy한 기타 커버곡 플레이리스트 좋아하셨으면 좋겠네요 :)+ 아...",
+              directoryInfo: {
+                emoji: null,
+                id: 1367,
+                name: "playlist🎧",
+              },
+              favicon:
+                "https://www.youtube.com/s/desktop/cb471591/img/favicon_32x32.png",
+              id: 11973,
+              link: "https://www.youtube.com/watch?v=C7sLwDOL1Vo",
+              provider: "YouTube",
+              readCnt: 1,
+              thumbnail: "https://i.ytimg.com/vi/C7sLwDOL1Vo/hqdefault.jpg",
+              title:
+                "[playlist]봄 밤, 창이 열린 1층 카페 끝 자리, 가사 없는 chilling 음악",
+            },
+            {
+              content:
+                "이번 플리는 오래전부터 좋아했던 아티스트 'Justice Der'의 기타 커버곡으로 이루어져 있습니다. 음악을 듣다 보면 한번쯤은 들어보셨을 만한 유명한 팝송 위주로 선곡했습니다. 봄밤과 어울리는 은은한 분위기의 jazzy한 기타 커버곡 플레이리스트 좋아하셨으면 좋겠네요 :)+ 아...",
+              directoryInfo: {
+                emoji: null,
+                id: 1367,
+                name: "playlist🎧",
+              },
+              favicon:
+                "https://www.youtube.com/s/desktop/cb471591/img/favicon_32x32.png",
+              id: 11973,
+              link: "https://www.youtube.com/watch?v=C7sLwDOL1Vo",
+              provider: "YouTube",
+              readCnt: 1,
+              thumbnail: "https://i.ytimg.com/vi/C7sLwDOL1Vo/hqdefault.jpg",
+              title:
+                "[playlist]봄 밤, 창이 열린 1층 카페 끝 자리, 가사 없는 chilling 음악",
+            },
+          ]}
+        />
+      </ShareCntnr>
+    </div>
+  );
+};
+export default Share;
+
+const ShareCntnr = styled.div`
+  width: 159.6rem;
+  ${({ theme }) => theme.media.desktop_2`
+    width: 127.2rem;
+  `}
+  /* 1366- 1599*/
+  ${({ theme }) => theme.media.desktop_3`
+    width: 115.2rem;
+  `}  
+  /* 1024-1365 */
+  ${({ theme }) => theme.media.desktop_4`
+    width: 85.8rem;
+  `}
+  /* 600-1023 */
+  ${({ theme }) => theme.media.tablet`
+    width: 56.4rem;
+  `}
+  /* -599 */
+   ${({ theme }) => theme.media.mobile`
+    width: 33.4rem;
+  `}
+`;
+
+const Title = styled.article`
+  margin-bottom: 5rem;
+
+  display: flex;
+  flex-direction: column;
+  .name {
+    margin: 0;
+
+    font-style: normal;
+    font-weight: bold;
+    font-size: 24px;
+    line-height: 36px;
+
+    color: var(--black_2);
+  }
+  .info {
+    margin: 0;
+
+    font-style: normal;
+    font-weight: 500;
+    font-size: 18px;
+    line-height: 36px;
+
+    color: var(--black_1);
+  }
+`;
+const User = styled.article`
+  margin-bottom: 3rem;
+
+  display: flex;
+  gap: 1rem;
+  & > img {
+    width: 2.8rem;
+    height: 2.8rem;
+    border-radius: 50%;
+  }
+  & > p {
+    margin: 0;
+
+    font-style: normal;
+    font-weight: 500;
+    font-size: 16px;
+    line-height: 2.8rem;
+
+    color: var(--black_1);
+  }
+`;
