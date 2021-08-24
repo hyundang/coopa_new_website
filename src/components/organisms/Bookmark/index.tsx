5d510df191e825db262d980c534bd860406ae71f
hyundang <hyunjin5697@yonsei.ac.kr>
2021-08-24 21:30:52 +0900

    refactor: Bookmark 단축키 추가
    - shift+b -> BookmarkAddModal 열림

--- src/components/organisms/Bookmark/index.tsx
+++ src/components/organisms/Bookmark/index.tsx
@@ -1,9 +1,11 @@
 import { BookmarkTile, Bubble } from "@components/atoms";
-import { BookmarkDataProps } from "@interfaces/homeboard";
-import { useState } from "react";
+import {
+  BookmarkDataProps,
+  PostBookmarkDataProps,
+} from "@interfaces/homeboard";
+import { useEffect, useState } from "react";
 import styled, { css } from "styled-components";
 import { BookmarkAddModal } from "..";
-import { NewBookmarkProps } from "../BookmarkAddModal";
 
 export interface BookmarkProps {
   /** id */
@@ -13,7 +15,7 @@ export interface BookmarkProps {
   /** bookmark data list */
   datas: BookmarkDataProps[];
   /** bookmark 추가 함수 */
-  onClickSave: (newBookmark: NewBookmarkProps) => Promise<void>;
+  onClickSave: (newBookmark: PostBookmarkDataProps) => Promise<void>;
   /** bookmark 삭제 함수 */
   onClickDel: (bookmarkID: number) => Promise<void>;
 }
@@ -29,10 +31,25 @@ const Bookmark = ({
   // bookmark add modal open 여부
   const [isOpen, setIsOpen] = useState(false);
   // 추가할 bookmark data
-  const [newBookmark, setNewBookmark] = useState<NewBookmarkProps>({
+  const [newBookmark, setNewBookmark] = useState<PostBookmarkDataProps>({
     name: "",
     link: "",
   });
+
+  // 키 떼어냈을 때
+  const handleKeyUp = (e: any) => {
+    // shift + b = 북마크 추가 모달
+    if (e.key === "B" && e.shiftKey) {
+      setIsOpen(true);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("keyup", handleKeyUp);
+    return () => {
+      window.removeEventListener("keyup", handleKeyUp);
+    };
+  }, []);
 
   return (
     <>
