ca94977bd25ba92602f57c005e42dfdfcfa18d75
hyundang <hyunjin5697@yonsei.ac.kr>
2021-09-22 01:31:35 +0900

    feat: toastMsg interface 추가

---
+++ src/lib/interfaces/toastMsg.ts
@@ -0,0 +1,11 @@
+export interface ToastMsgProps {
+  dirCreate: boolean;
+  dirDel: boolean;
+  dirEdit: boolean;
+  cookieDel: boolean;
+  cookieEdit: boolean;
+  bookmarkDel: boolean;
+  bookmarkCreate: boolean;
+  homeboardEdit: boolean;
+  imgSizeOver: boolean;
+}
