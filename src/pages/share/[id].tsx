7772421729595423e0604d50a665268cd5a02d1c
hyundang <hyunjin5697@yonsei.ac.kr>
2021-09-22 04:09:31 +0900

    feat: SharePage api 연결
    - CookeModule, DirModule로 swr 분리
    - pre-rendering 방식 사용

--- src/pages/share/[id].tsx
+++ src/pages/share/[id].tsx
@@ -1,98 +1,73 @@
 import { DirDetail } from "@components/templates";
-import { useRouter } from "next/router";
+import nextCookie from "next-cookies";
 import getApi from "@api/getApi";
-import useSWR from "swr";
-import { useState } from "react";
 import { readCountAsc, readCountDesc } from "@lib/filter";
-import { CookieDataProps } from "@interfaces/cookie";
+import { SharedDirectoryCookieDataProps } from "@interfaces/cookie";
+import CookieModule from "@modules/CookieModule";
+import { useToastMsg } from "src/hooks";
 
-const SharePage = () => {
-  const router = useRouter();
+interface SharePageProps {
+  initSharedDirDetailData: SharedDirectoryCookieDataProps;
+  queryID: number;
+}
+const SharePage = ({ initSharedDirDetailData, queryID }: SharePageProps) => {
+  // toast msg visible state
+  const { isVisible, setIsVisible } = useToastMsg();
 
-  // 공유 디렉토리 안 쿠키 데이터 get
-  const { data: sharedDirCookieData } = useSWR(
-    `/share/${router.query.id}`,
-    getApi.getSharedDirectoryData,
-    {
-      onErrorRetry: ({ retryCount }) => {
-        // 3번 까지만 재시도함
-        if (retryCount >= 3) return undefined;
-        return true;
-      },
-      onSuccess: (data) => {
-        const filter = localStorage.getItem("cookieFilter");
-        if (data && filter !== null) {
-          switch (filter) {
-            case "readMost":
-              setCookieFilter("readMost");
-              setFilteredCookieData([...data.cookies].sort(readCountDesc));
-              break;
-            case "readLeast":
-              setCookieFilter("readLeast");
-              setFilteredCookieData([...data.cookies].sort(readCountAsc));
-              break;
-            case "oldest":
-              setCookieFilter("oldest");
-              setFilteredCookieData([...data.cookies].reverse());
-              break;
-            default:
-              setCookieFilter("latest");
-              setFilteredCookieData([...data.cookies]);
-              break;
-          }
-        }
-      },
-    },
-  );
-  // 쿠키 필터
-  const [cookieFilter, setCookieFilter] = useState<
-    "latest" | "readMost" | "readLeast" | "oldest"
-  >("latest");
-  // 필터링 된 쿠키 데이터
-  const [filteredCookieData, setFilteredCookieData] = useState<
-    CookieDataProps[] | undefined
-  >([]);
-
-  // 쿠키 필터 변경
-  const handleCookieFilter = (
-    filter: "latest" | "readMost" | "readLeast" | "oldest" | "abc",
-  ) => {
-    filter !== "abc" && setCookieFilter(filter);
-    localStorage.setItem("cookieFilter", filter);
-    switch (filter) {
-      case "readMost":
-        setFilteredCookieData(
-          sharedDirCookieData &&
-            [...sharedDirCookieData.cookies].sort(readCountDesc),
-        );
-        break;
-      case "readLeast":
-        setFilteredCookieData(
-          sharedDirCookieData &&
-            [...sharedDirCookieData.cookies].sort(readCountAsc),
-        );
-        break;
-      case "oldest":
-        setFilteredCookieData(
-          sharedDirCookieData && [...sharedDirCookieData.cookies].reverse(),
-        );
-        break;
-      default:
-        setFilteredCookieData(sharedDirCookieData?.cookies);
-        break;
-    }
-  };
+  // 쿠키 모듈
+  const cookieModule = CookieModule({
+    key: `/share/${queryID}`,
+    initAllCookieData: initSharedDirDetailData.cookies,
+    isVisible,
+    setIsVisible,
+  });
 
   return (
     <DirDetail
       isShared
-      imgUrl={sharedDirCookieData?.userInfo.profileImage}
-      nickname={sharedDirCookieData?.userInfo.name || ""}
-      dirInfo={sharedDirCookieData?.directoryInfo || { name: "", id: 0 }}
-      cookies={filteredCookieData || []}
-      filterType={cookieFilter}
-      onClickType={handleCookieFilter}
+      imgUrl={initSharedDirDetailData?.userInfo.profileImage}
+      nickname={initSharedDirDetailData?.userInfo.name || ""}
+      dirInfo={initSharedDirDetailData?.directoryInfo || { name: "", id: 0 }}
+      cookies={cookieModule.filteredCookieData || []}
+      filterType={cookieModule.cookieFilter}
+      onClickType={cookieModule.handleCookieFilter}
+      isToastMsgVisible={isVisible}
+      setIsToastMsgVisible={setIsVisible}
+      delCookieHandler={cookieModule.handleDelCookie}
+      handleEditCookie={cookieModule.handleEditCookie}
+      handleDirAddCookie={cookieModule.handleAddCookieToDir}
     />
   );
 };
 export default SharePage;
+
+SharePage.getInitialProps = async (ctx: any) => {
+  const allCookies = nextCookie(ctx);
+  const queryID = ctx.query.id;
+
+  // 쿠키 데이터
+  const initSharedDirDetailData = await getApi.getSharedDirectoryData(
+    `/share/${queryID}`,
+  );
+  const { cookieFilter } = allCookies;
+  if (cookieFilter) {
+    switch (cookieFilter) {
+      case "readMost":
+        initSharedDirDetailData?.cookies.sort(readCountDesc);
+        break;
+      case "readLeast":
+        initSharedDirDetailData?.cookies.sort(readCountAsc);
+        break;
+      case "oldest":
+        initSharedDirDetailData?.cookies.reverse();
+        break;
+      default:
+        break;
+    }
+  }
+
+  return {
+    initSharedDirDetailData,
+    queryID,
+  };
+};
