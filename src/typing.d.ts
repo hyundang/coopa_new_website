d5b25f2735dd80c2aabf76061e9ae547f0a4db3d
hyundang <hyunjin5697@yonsei.ac.kr>
2021-11-07 00:52:32 +0900

    chore: define "COOKIE_PAGE_SIZE"

--- src/typing.d.ts
+++ src/typing.d.ts
@@ -23,6 +23,7 @@ declare module "*.gif" {
 declare const DOMAIN: string;
 declare const HTTP_ONLY: string;
 declare const API_DOMAIN: string;
+declare const COOKIE_PAGE_SIZE: number;
 
 declare module "axios" {
   export interface AxiosRequestConfig {
