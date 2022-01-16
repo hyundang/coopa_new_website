8a6e1b451050b2d4394e7a4c4d50d90acbf5a6a7
hyundang <hyunjin5697@yonsei.ac.kr>
2022-01-16 21:24:06 +0900

    chore: asset 추가

--- src/assets/icons/card/index.ts
+++ src/assets/icons/card/index.ts
@@ -3,5 +3,7 @@ export { ReactComponent as FaviconIcon } from "./icn_favicon.svg";
 export { ReactComponent as DropDownIcon } from "./icn_dropdown.svg";
 export { ReactComponent as DefaultEmojiIcon } from "./icn_cookiehover_noicn.svg";
 export { ReactComponent as PinIcon } from "./icn_cookiehover_pin.svg";
+export { ReactComponent as PinIconGray } from "./icn_cookiehover_pin_gray.svg";
 export { ReactComponent as PinAtvIcon } from "./icn_cookiehover_pin_active.svg";
+export { ReactComponent as PinAtvIconGray } from "./icn_cookiehover_pin_active_gray.svg";
 export { default as fvcOnErrorImg } from "./icn_favicon.svg";
