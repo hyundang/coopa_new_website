b9c735a64fee9541b159e6f5f168e63f5f7dab79
hyundang <hyunjin5697@yonsei.ac.kr>
2022-01-17 21:43:41 +0900

    refacotor: CookieMobile, CookieTablet에서 cookie content 부분 분리

--- src/components/organisms/CookieTablet/index.tsx
+++ src/components/organisms/CookieTablet/index.tsx
@@ -1,48 +1,33 @@
 // assets
-import { DeleteIcon, PinAtvIconGray, PinIconGray } from "@assets/icons/card";
-import { EditIcon, LinkIcon32 } from "@assets/icons/common";
 import { NoThumbImg, PinImg } from "@assets/imgs/card";
 // components
-import { Icon } from "@components/atoms";
 import { CookieEditModal, DelModal } from "@components/organisms";
+import { CookieContent } from "@components/molecules";
 // interfaces
-import { UpdateCookieProps, CookieDataProps } from "@interfaces/cookie";
+import { CookieDataProps } from "@interfaces/cookie";
 // libs
 import styled from "styled-components";
-import React, { useState, SyntheticEvent, RefObject, forwardRef } from "react";
-import { CopyToClipboard } from "react-copy-to-clipboard";
+import React, { SyntheticEvent, RefObject, forwardRef } from "react";
+import CookieHandlers from "@lib/CookieHandlers";
 // modules
 import CookieModule from "@modules/CookieModule";
 
 export interface CookieMobileProps {
-  /** id */
   id?: string;
-  /** className */
   className?: string;
-  /** cookie type */
   type: "normal" | "searched" | "dirDetail" | "dirShare";
-  /** cookie */
-  cookie: CookieDataProps;
-  /** 쿠키 모듈 */
+  cookieData: CookieDataProps;
   cookieModule: ReturnType<typeof CookieModule>;
 }
 const CookieMobile = (
-  { id, className, type, cookie, cookieModule }: CookieMobileProps,
+  { id, className, type, cookieData, cookieModule }: CookieMobileProps,
   ref?:
     | ((instance: HTMLButtonElement | null) => void)
     | RefObject<HTMLButtonElement>
     | null
     | undefined,
 ) => {
-  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
-  const [isDelModalOpen, setIsDelModalOpen] = useState(false);
-
-  const [cookieValue, setCookieValue] = useState<UpdateCookieProps>({
-    title: cookie.title,
-    content: cookie.content,
-    cookieId: cookie.id,
-    thumbnail: cookie.thumbnail,
-  });
+  const cookieHandlers = CookieHandlers(type, cookieData, cookieModule);
 
   return (
     <>
@@ -51,93 +36,59 @@ const CookieMobile = (
         className={className}
         onClick={() => {
           cookieModule.updateCookieReadCnt(
-            cookie.id,
-            cookie.isPinned,
+            cookieData.id,
+            cookieData.isPinned,
             type === "searched",
           );
-          window.open(cookie.link);
+          window.open(cookieData.link);
         }}
         ref={ref}
       >
         <div className="thumbnail-wrap">
-          {cookie.isPinned && <StyledPinImg className="pin_img" />}
+          {cookieData.isPinned && <StyledPinImg className="pin_img" />}
           <img
             alt="cookie-thumbnail"
             className="thumbnail"
-            src={cookie.thumbnail}
+            src={cookieData.thumbnail}
             onError={(e: SyntheticEvent<HTMLImageElement, Event>) =>
               (e.currentTarget.src = NoThumbImg)
             }
           />
         </div>
-        <section className="content">
-          <h1 className="title">{cookie.title}</h1>
-          <p className="desc">{cookie.content}</p>
-          <div className="profile">
-            <cite className="profile__site">{cookie.provider}</cite>
-            <div style={{ flexGrow: 1 }} />
-            {type !== "dirShare" && (
-              <div className="profile__icon-wrap">
-                <Icon
-                  className="icon"
-                  onClick={() =>
-                    cookieModule.updateCookiePin(
-                      cookie.id,
-                      cookie.isPinned,
-                      type === "searched",
-                    )
-                  }
-                >
-                  {cookie.isPinned ? (
-                    <PinAtvIconGray className="icon__pin" />
-                  ) : (
-                    <PinIconGray className="icon__pin" />
-                  )}
-                </Icon>
-                <Icon className="icon" onClick={() => setIsEditModalOpen(true)}>
-                  <EditIcon className="icon__asset" />
-                </Icon>
-                <CopyToClipboard
-                  text={cookie.link}
-                  onCopy={cookieModule.copyCookieLink}
-                >
-                  <Icon className="icon">
-                    <LinkIcon32 className="icon__asset--link" />
-                  </Icon>
-                </CopyToClipboard>
-                <Icon className="icon" onClick={() => setIsDelModalOpen(true)}>
-                  <DeleteIcon className="icon__asset" />
-                </Icon>
-              </div>
-            )}
-          </div>
-        </section>
+        <CookieContent
+          type={type}
+          cookieData={cookieData}
+          onClickPinIcon={cookieHandlers.handleClickPinIcon}
+          onClickEditIcon={() => cookieHandlers.setIsEditModalOpen(true)}
+          onClickShareIcon={cookieModule.copyCookieLink}
+          onClickDelIcon={() => cookieHandlers.setIsDelModalOpen(true)}
+        />
       </CookieWrap>
       <CookieEditModal
-        isOpen={isEditModalOpen}
-        setIsOpen={setIsEditModalOpen}
-        value={cookieValue}
-        setValue={setCookieValue}
+        isOpen={cookieHandlers.isEditModalOpen}
+        setIsOpen={cookieHandlers.setIsEditModalOpen}
+        value={cookieHandlers.updatedCookieValue}
+        setValue={cookieHandlers.setUpdatedCookieValue}
         updateCookie={(formData) =>
           cookieModule.updateCookie(
             formData,
-            cookie.isPinned,
+            cookieData.isPinned,
             type === "searched",
           )
         }
         onClickDel={() => {
-          setIsEditModalOpen(false);
-          setIsDelModalOpen(true);
+          cookieHandlers.setIsEditModalOpen(false);
+          cookieHandlers.setIsDelModalOpen(true);
         }}
         isLoading={cookieModule.isUpdateLoading}
       />
       <DelModal
-        isOpen={isDelModalOpen}
-        setIsOpen={setIsDelModalOpen}
+        isOpen={cookieHandlers.isDelModalOpen}
+        setIsOpen={cookieHandlers.setIsDelModalOpen}
         onClickDel={() =>
           cookieModule.deleteCookie(
-            cookie.id,
-            cookie.isPinned,
+            cookieData.id,
+            cookieData.isPinned,
             type === "searched",
           )
         }
@@ -164,78 +115,6 @@ const CookieWrap = styled.article`
       height: 136px;
       border-radius: 8px;
       object-fit: cover;
-    }
-  }
-
-  .content {
-    display: flex;
-    flex-direction: column;
-    flex-grow: 1;
-
-    .title {
-      all: unset;
-      color: var(--black_1);
-      line-height: 25px;
-      font-size: 16px;
-      font-weight: 500;
-      display: -webkit-box;
-      -webkit-box-orient: vertical;
-      -webkit-line-clamp: 2;
-      overflow: hidden;
-      word-wrap: normal;
-      word-break: break-all;
-    }
-
-    .desc {
-      all: unset;
-      margin-top: 6px;
-      font-weight: 400;
-      line-height: 22px;
-      font-size: 14px;
-      display: -webkit-box;
-      -webkit-box-orient: vertical;
-      -webkit-line-clamp: 2;
-      overflow: hidden;
-      word-wrap: normal;
-      word-break: break-all;
-      color: var(--gray_5);
-    }
-
-    .profile {
-      margin-top: 16px;
-      display: flex;
-      flex-direction: row;
-      align-items: center;
-      justify-content: space-between;
-      &__site {
-        all: unset;
-        font-size: 13px;
-        color: var(--gray_5);
-        display: -webkit-box;
-        -webkit-box-orient: vertical;
-        -webkit-line-clamp: 1;
-        overflow: hidden;
-        word-wrap: normal;
-        word-break: break-all;
-      }
-      &__icon-wrap {
-        display: flex;
-        flex-direction: row;
-        .icon {
-          width: 36px;
-          height: 32px;
-          &__asset {
-            path {
-              fill: var(--gray_5);
-            }
-          }
-          &__asset--link {
-            path {
-              stroke: var(--gray_5);
-            }
-          }
-        }
-      }
     }
   }
 `;
