19b4beb66744f21161f0d7086a830dabf57da2c5
hyundang <hyunjin5697@yonsei.ac.kr>
2022-01-18 02:49:11 +0900

    chore: client id 전역으로 선언

--- src/global.d.ts
+++ src/global.d.ts
@@ -3,3 +3,4 @@ declare const HTTP_ONLY: string;
 declare const API_DOMAIN: string;
 declare const COOKIE_PAGE_SIZE: number;
 declare const VERSION: string;
+declare const CLIENT_ID: string;
--- src/typing.d.ts
+++ src/typing.d.ts
@@ -7,6 +7,7 @@ declare const HTTP_ONLY: string;
 declare const API_DOMAIN: string;
 declare const COOKIE_PAGE_SIZE: number;
 declare const VERSION: string;
+declare const CLIENT_ID: string;
 
 declare module "axios" {
   export interface AxiosRequestConfig {
