dd05e90427a2de75498fbb5b824bfca803b564a8
hyundang <hyunjin5697@yonsei.ac.kr>
2022-01-23 00:56:14 +0900

    fix: atom components stories

--- src/components/atoms/SearchBar/SearchBar.stories.tsx
+++ src/components/atoms/SearchBar/SearchBar.stories.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { Meta, Story } from "@storybook/react";
-import SearchBar, { SearchBarProps } from ".";
+import SearchBar from ".";
 
 export default {
   component: SearchBar,
   title: "components/atoms/SearchBar",
 } as Meta;
 
-export const Default: Story<SearchBarProps> = (args) => {
+export const Default: Story = (args) => {
   return (
     <div style={{ width: "100%", padding: "0 20px" }}>
       <SearchBar {...args} />
