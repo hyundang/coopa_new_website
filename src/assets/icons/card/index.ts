57f277532d65b824aab66fe7c3c4e281f487118d
hyundang <hyunjin5697@yonsei.ac.kr>
2022-01-23 00:24:36 +0900

    chore: img asset change

--- src/assets/icons/card/index.ts
+++ src/assets/icons/card/index.ts
@@ -1,9 +1,9 @@
 export { ReactComponent as DeleteIcon } from "./icn_delete_white_32px.svg";
-export { ReactComponent as FaviconIcon } from "./icn_favicon.svg";
 export { ReactComponent as DropDownIcon } from "./icn_dropdown.svg";
 export { ReactComponent as DefaultEmojiIcon } from "./icn_cookiehover_noicn.svg";
 export { ReactComponent as PinIcon } from "./icn_cookiehover_pin.svg";
 export { ReactComponent as PinIconGray } from "./icn_cookiehover_pin_gray.svg";
 export { ReactComponent as PinAtvIcon } from "./icn_cookiehover_pin_active.svg";
 export { ReactComponent as PinAtvIconGray } from "./icn_cookiehover_pin_active_gray.svg";
-export { default as fvcOnErrorImg } from "./icn_favicon.svg";
+export { default as FaviconIcon } from "./icn_favicon.svg";
+export { default as fvcOnErrorImg } from "./icn_favicon_error.svg";
